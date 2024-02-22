29a2fc0efa36b2f776bd316bd5a735555c8abb9b
Colleen Dixon <cdixon@3-gis.com>
2024-02-21 18:32:52 -0600

    main - initial commit

---
+++ Db3Parser/ParseDb3Files.cs
@@ -0,0 +1,38 @@
+﻿namespace Db3Parser;
+
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+
+class ParseDb3Files
+{
+    public static void Parse(string table, string column, string value, List<string> dbPaths)
+    {
+        Console.WriteLine("The following db3 files match the criteria:");
+        Console.WriteLine();
+        
+        foreach (string dbPath in dbPaths)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection($"Data Source={dbPath};Version=3;"))
+            {
+                connection.Open();
+                
+                var query = $"Select * from {table} where NetworkName='Telecom Network' and {column}={value}";
+
+                using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                {
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var mapServiceUrl = reader.GetString(reader.GetOrdinal("MapServiceUrl"));
+                            var networkId = reader.GetInt32(reader.GetOrdinal("NetworkId"));
+
+                            Console.WriteLine($"{Path.GetFileName(dbPath)} has {column}: {value} for Telecom Network on map service: {mapServiceUrl}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Db3Parser/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Db3Parser;
+
+Console.WriteLine("Enter folder location of db3 files:");
+var folderLocation = Console.ReadLine();
+
+if (folderLocation == null)
+{
+    Console.WriteLine("Invalid folder location, exiting.");
+    return;
+}
+Console.WriteLine("By passing in the table, column(s) and value(s), this program will return the files that match those criteria.  For example, to find all files that have NotifyCircuitId set to true in NetworkConfiguration, pass in the following: Table: NetworkConfiguration, Column: NotifyCircuitId, Value: 1");
+Console.WriteLine();
+
+Console.WriteLine("Enter Table:");
+var table = Console.ReadLine();
+Console.WriteLine();
+
+Console.WriteLine("Enter Column:");
+var column = Console.ReadLine();
+Console.WriteLine();
+
+Console.WriteLine("Enter Value:");
+var value = Console.ReadLine();
+Console.WriteLine();
+
+if (table == null || column == null || value == null)
+    return;
+
+var sqlQuery = $"Select * from {table} where {column}={value}";
+
+Console.WriteLine($"Processing {sqlQuery}...");
+Console.WriteLine();
+
+try
+{
+    string[] files = Directory.GetFiles(folderLocation);
+
+    // Filter the files to keep only the .db3 files
+    List<string> db3Files = files
+        .Where(file => Path.GetExtension(file).Equals(".db3", StringComparison.OrdinalIgnoreCase)).ToList();
+
+    ParseDb3Files.Parse(table, column, value, db3Files);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
+finally
+{
+    Console.WriteLine();
+    Console.WriteLine("Press any key to exit...");
+    Console.ReadKey();
+}
